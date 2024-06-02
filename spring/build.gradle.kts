b1a5365b6c147454beb4d077ab7907824ba4d137
DevHyeong <44819285+DevHyeong@users.noreply.github.com>
2024-06-02 10:01:57 +0900

    Create build.gradle.kts

---
+++ spring/build.gradle.kts
@@ -0,0 +1,15 @@
+
+// spring cloud 적용법
+
+val springCloudVersion = "2023.0.1"
+
+the<DependencyManagementExtension>().apply {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+}
+
